4bbeea17a8e14a0d8ac0097a1dfc521dd410f112
ethanJH24 <ethanjoeharison@gmail.com>
2026-02-11 19:37:55 +0000

    main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const toggleTheme = () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.classList.contains("dark"));
+};
+
+if (localStorage.getItem("theme") === "true") {
+  document.body.classList.add("dark");
+}
